4a09a3fff58b5666b27fddc2f2570719ce538e69
Przemek Jakubczyk <pjakubczyk@applause.com>
2015-08-27 16:20:56 +0200

    Erase part 3 answers

--- toast/src/test/groovy/org/jakubczyk/codepot/ToastActivityRobolectricSpec.groovy
+++ toast/src/test/groovy/org/jakubczyk/codepot/ToastActivityRobolectricSpec.groovy
@@ -10,32 +10,23 @@ class ToastActivityRobolectricSpec extends RoboSpecification {
 
     def "should assign listener to the label"() {
         given: "create activity controller"
-        def activityController = Robolectric.buildActivity(ToastActivity)
 
         and: "get the activity from controller"
-        def activity = activityController.get()
 
         when: "make controller call onCreate"
-        activityController.create()
 
         then: "check if correct text is set"
-        Robolectric.shadowOf(activity.labelTv).getOnClickListener()
     }
 
     def "should show a toast on click event"() {
         given: "create activity controller"
-        def activityController = Robolectric.buildActivity(ToastActivity)
 
         and: "get the activity from controller"
-        def activity = activityController.get()
 
         and: "make controller call onCreate"
-        activityController.create()
 
         when: "call action, click on the label"
-        activity.labelTv.performClick()
 
         then: "check if correct text in the Toast is set"
-        ShadowToast.textOfLatestToast =~ "Cli"
     }
 }
--- toast/src/test/groovy/org/jakubczyk/codepot/ToastActivitySpec.groovy
+++ toast/src/test/groovy/org/jakubczyk/codepot/ToastActivitySpec.groovy
@@ -1,36 +1,26 @@
 package org.jakubczyk.codepot
 
-import android.widget.TextView
 import spock.lang.Specification
-
 
 class ToastActivitySpec extends Specification {
 
     def "should assign listener to the label"() {
         given: "create activity"
-        def activity = new ToastActivity()
 
         and: "mock the widget"
-        activity.labelTv = Mock(TextView)
 
         when: "call action, setListeners()"
-        activity.setListeners()
 
         then: "check if listener was added"
-        1 * activity.labelTv.setOnClickListener(activity)
     }
 
     def "should show a toast on click event"() {
         given: "create activity"
-        def activity = new ToastActivity()
 
         and: "mock the toaster"
-        activity.toaster = Mock(Toaster)
 
         when: "call action, onClick()"
-        activity.onClick(null)
 
         then: "check if toaster showed text once"
-        1 * activity.toaster.show("Clicked!")
     }
 }
