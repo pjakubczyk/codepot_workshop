7405a20420f859a6f74fcad248f893c3300e2fb9
Przemek Jakubczyk <pjakubczyk@applause.com>
2015-08-27 16:18:49 +0200

    Erase part 1 answers

--- activity/src/test/groovy/org/jakubczyk/codepot/MainActivitySpec.groovy
+++ activity/src/test/groovy/org/jakubczyk/codepot/MainActivitySpec.groovy
@@ -9,92 +9,62 @@ class MainActivitySpec extends Specification {
 
     def "should fail with find view by id"() {
         given: "create an activity"
-        def activity = new MainActivity()
 
         when: "call on create"
-        activity.onCreate(null)
 
         then: "check if widget is empty"
-        !activity.titleTv
     }
 
     def "should show low value"() {
         given: "create an activity"
-        def activity = new MainActivity()
 
         and: "set the conditional value"
-        activity.conditionValue = 2
 
         and: "mock widget"
-        activity.titleTv = Mock(TextView)
 
         when: "resume activity"
-        activity.onResume()
 
         then: "check if correct text is set"
-        1 * activity.titleTv.setText("Low Number")
     }
 
     def "should show high value"() {
         given: "create an activity"
-        def activity = new MainActivity()
 
         and: "set the conditional value"
-        activity.conditionValue = 4
 
         and: "mock widget"
-        activity.titleTv = Mock(TextView)
 
         when: "resume activity"
-        activity.onResume()
 
         then: "check if correct text is set"
-        1 * activity.titleTv.setText("High Number")
     }
 
     // The Intent
 
     def "should show that bundle is empty"() {
         given: "create an activity"
-        def activity = new MainActivity()
 
         and: "mock starting Intent"
-        activity.startingIntent = Mock(Intent) {
-            getExtras() >> null
-        }
 
         and: "mock widget"
-        activity.labelTv = Mock(TextView)
 
         when: "call action"
-        activity.processExtras()
 
         then: "check if correct text is set"
-        1 * activity.labelTv.setText("Null Bundle")
     }
 
     def "should show bundle size"() {
         given: "create an activity"
-        def activity = new MainActivity()
 
         and: "mock bundle, make size() return 2"
-        def bundle = Mock(Bundle) {
-            size() >> 2
-        }
 
         and: "mock starting intent, make getExtras() return bundle"
-        activity.startingIntent = Mock(Intent) {
-            getExtras() >> bundle
-        }
 
         and: "mock widget"
-        activity.labelTv = Mock(TextView)
 
         when: "call action"
-        activity.processExtras()
 
         then: "check if correct text is set"
-        1 * activity.labelTv.setText("Bundle with 2 elements")
     }
 
 
--- activity/src/test/groovy/org/jakubczyk/codepot/MainActivitySpecRobolectric.groovy
+++ activity/src/test/groovy/org/jakubczyk/codepot/MainActivitySpecRobolectric.groovy
@@ -12,89 +12,62 @@ class MainActivitySpecRobolectric extends RoboSpecification {
 
     def "should build the activity"() {
         given: "create activity controller"
-        def activityController = Robolectric.buildActivity(MainActivity)
 
         and: "get the activity from controller"
-        def activity = activityController.get()
 
         when: "make controller call onCreate"
-        activityController.create()
 
         then: "check if widget is not empty"
-        activity.titleTv
     }
 
     def "should show low value"(){
         given: "create activity controller"
-        def activityController = Robolectric.buildActivity(MainActivity)
 
         and: "get the activity from controller"
-        def activity = activityController.get()
 
         and: "set conditional value to 2"
-        activity.conditionValue = 2
 
         when: "make controller call onCreate and onResume"
-        activityController.create().resume()
 
         then: "check if correct text is set"
-        activity.titleTv.text =~ "Low"
     }
 
     def "should show high value"(){
         given: "create activity controller"
-        def activityController = Robolectric.buildActivity(MainActivity)
 
         and: "get the activity from controller"
-        def activity = activityController.get()
 
         and: "set conditional value to 4"
-        activity.conditionValue = 4
 
         when: "make controller call onCreate and onResume"
-        activityController.create().resume()
 
         then: "check if correct text is set"
-        activity.titleTv.text =~ "High"
     }
 
     // The Intent
 
     def "should show that bundle is empty"() {
         given: "create activity controller with intent and call create"
-        def activityController = Robolectric.buildActivity(MainActivity).withIntent(new Intent()).create()
 
         and: "get the activity from controller"
-        def activity = activityController.get()
 
         when: "call action"
-        activity.processExtras()
 
         then: "check if correct text is set"
-        activity.labelTv.text == "Null Bundle"
     }
 
     def "should show bundle size"(){
         given: "fill bundle with elements"
-        def bundle = new Bundle()
-        bundle.putString("key1", "v1")
-        bundle.putString("key2", "v2")
 
         and: "pass it to the intent"
-        def intent = new Intent()
-        intent.putExtras(bundle)
 
         and: "create activity controller with intent and call create"
-        def activityController = Robolectric.buildActivity(MainActivity).withIntent(intent).create()
 
         and: "get the activity from controller"
-        def activity = activityController.get()
 
         when: "call action"
-        activity.processExtras()
 
         then: "check if correct text is set"
-        activity.labelTv.text == "Bundle with 2 elements"
     }
 
 }
