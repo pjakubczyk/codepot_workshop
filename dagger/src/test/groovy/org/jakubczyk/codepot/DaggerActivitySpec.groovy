3e1496d0394ab95405dad58d9d693cff525a51e3
Przemek Jakubczyk <pjakubczyk@applause.com>
2015-08-27 16:20:07 +0200

    Erase part 2 answers

--- dagger/src/test/groovy/org/jakubczyk/codepot/DaggerActivitySpec.groovy
+++ dagger/src/test/groovy/org/jakubczyk/codepot/DaggerActivitySpec.groovy
@@ -12,85 +12,56 @@ class DaggerActivitySpec extends Specification {
     // use default Module
     def "should show first user"() {
         given: "create an activity"
-        def activity = new DaggerActivity()
 
         and: "set a new user"
-        activity.user = new User([name: "Adam", fromPackage: "org.jakubczyk"])
 
         and: "mock the widget"
-        activity.titleTv = Mock(TextView)
 
         when: "call onResume"
-        activity.onResume()
 
         then: "check if correct text is set"
-        1 * activity.titleTv.setText("name=Adam fromPackage=org.jakubczyk")
     }
 
     def "should show third user"() {
         given: "prepare Dagger Injector"
-        DaggerInjector.start(new DaggerModule(Mock(Context)))
 
         and: "create an activity"
-        def activity = new DaggerActivity()
 
         and: "mock the widget"
-        activity.labelTv = Mock(TextView)
 
         when: "call action, update Users"
-        activity.updateUsers()
 
         then: "check if first no 0 is set"
-        1 * activity.labelTv.setText("name=Adam no. 0 fromPackage=null")
 
         then: "check if first no 1 is set"
-        1 * activity.labelTv.setText("name=Adam no. 1 fromPackage=null")
 
         then: "check if first no 2 is set"
-        1 * activity.labelTv.setText("name=Adam no. 2 fromPackage=null")
     }
 
     // replace "Component"
     def "should show first user with component"() {
         given: "Override DaggerInjector component with a Mock"
-        DaggerInjector.component = Mock(DaggerComponent) {
-            inject(_) >> { arguments ->
-                arguments[0].user = new User([name: "Adam", fromPackage: "org.jakubczyk"])
-            }
-        }
 
         and: "create an activity"
-        def activity = new DaggerActivity()
 
         and: "call OnCreate"
-        activity.onCreate(null)
 
         and: "mock the widget"
-        activity.titleTv = Mock(TextView)
 
         when: "call onResume"
-        activity.onResume()
 
         then: "check if correct text is set"
-        1 * activity.titleTv.setText("name=Adam fromPackage=org.jakubczyk")
     }
 
     def "should show thrid user with component"() {
         given: "Override DaggerInjector component with a Mock"
-        DaggerInjector.component = Mock(DaggerComponent) {
-            getUser() >> new User([name: "Adam", fromPackage: "org.jakubczyk"])
-        }
 
         and: "create an activity"
-        def activity = new DaggerActivity()
 
         and: "mock the wigdet"
-        activity.labelTv = Mock(TextView)
 
         when: "call action, update Users()"
-        activity.updateUsers()
 
         then: "check if 3 times the label was set"
-        3 * activity.labelTv.setText("name=Adam fromPackage=org.jakubczyk")
     }
 }
--- dagger/src/test/groovy/org/jakubczyk/codepot/DaggerActivitySpecRobolectric.groovy
+++ dagger/src/test/groovy/org/jakubczyk/codepot/DaggerActivitySpecRobolectric.groovy
@@ -10,29 +10,21 @@ class DaggerActivitySpecRobolectric extends RoboSpecification {
 
     def "should show first user"() {
         given: "create activity controller"
-        def controller = Robolectric.buildActivity(DaggerActivity)
 
         and: "get the activity from controller"
-        def activity = controller.get()
 
         when: "make controller run create() resume()"
-        controller.create().resume()
 
         then: "check if correct text is set"
-        activity.titleTv.text =~ "Adam no. 0"
     }
 
     def "should show third user"() {
         given: "create activity controller"
-        def controller = Robolectric.buildActivity(DaggerActivity)
 
         and: "get the activity from controller"
-        def activity = controller.get()
 
         when: "make controller run create() resume()"
-        controller.create().resume()
 
         then: "check if correct text is set after thrid run"
-        activity.labelTv.text =~ "Adam no. 3"
     }
 }
